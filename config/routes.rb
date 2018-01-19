33c6fce1eaec2850ceed27de167969f4fd62ddd6
IrynaOvdii <irina.ovdij@gmail.com>
2018-01-19 06:17:26 +0100

    Add CarrierWave and generate image uploader

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on thRails.application.routes.draw do
+
+  resources :songs
+  root "artists#index"
+  resources :artists do
+    resources :songs
+  end
+
 end
