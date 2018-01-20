672938005974db23d36cc02994bdfa8f95ffab36
IrynaOvdii <irina.ovdij@gmail.com>
2018-01-20 12:12:53 +0100

    Remove any song associated to the artist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,16 +1,30 @@
 class SongsController < ApplicationController
   before_action :set_artist
 
-
   def show
-     @artist = Artist.find(params[:artist_id])
      @song = artist.songs.find(params[:artist_id])
   end
 
   def new
-    @artist = Artist.find(params[:artist_id])
     @song = @artist.songs.new
   end
+
+  def create
+    @song = Song.new(song_params)
+    @song.artist = Artist.find(params[:artist_id])
+    if @song.save
+      redirect_to @song.artist
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @song = @artist.songs.find(params[:id])
+    @song.destroy
+    redirect_to @artist, notice: "Song Deleted!"
+  end
+
 
 
 
@@ -18,7 +32,7 @@ class SongsController < ApplicationController
       @artist = Artist.find(params[:artist_id])
   end
 
-
-
-
+  def song_params
+    params.require(:song).permit(:name, :duration, :audio_url, :artist_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on thRails.application.routes.draw do
   root 'artists#index'
   resources :artists do
+    #delete '/:id/song' => 'songs#destroy', as: :set_artist
     resources :songs
   end
 
