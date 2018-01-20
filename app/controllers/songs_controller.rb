d9fc36f86a691de77a9fa0cc2bba4e8c1dddf920
IrynaOvdii <irina.ovdij@gmail.com>
2018-01-20 06:44:15 +0100

    Add a new song to the artist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,24 @@
 class SongsController < ApplicationController
+  before_action :set_artist
+
+
   def show
-     @artist = Artist.find(params[:id])
-     @song = artist.songs.find(params[:id])
-   end
+     @artist = Artist.find(params[:artist_id])
+     @song = artist.songs.find(params[:artist_id])
+  end
+
+  def new
+    @artist = Artist.find(params[:artist_id])
+    @song = @artist.songs.new
+  end
+
+
+
+  def set_artist
+      @artist = Artist.find(params[:artist_id])
+  end
+
+
 
 
 end
