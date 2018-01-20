b24de1ab18cdf9620fd338d3cd1283bbf5ed0dda
IrynaOvdii <irina.ovdij@gmail.com>
2018-01-20 18:00:49 +0100

    Create Photo: Controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,6 +6,40 @@ class ArtistsController < ApplicationController
 
   def show
     @artist = Artist.find(params[:id])
+    @photos = @artist.photos
+  end
+
+  def new
+    @artist = @artist.new
+  end
+
+  def create
+    @artist = artists.build(artist_params)
+
+      if @artist.save
+        image_params.each do |image|
+          @artist.photos.create(image: image)
+        end
+
+      redirect_to edit_artist_path(@artist), notice: "Artist successfully created"
+      else
+        render :new
+      end
+  end
+
+  def edit
+      @photos = @artist.photos
+  end
+
+  def update
+    if @artist.update(artist_params)
+      image_params.each do |image|
+        @artist.photos.create(image: image)
+    end
+      redirect_to edit_artist_path(@artist), notice: "Artist updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -13,6 +47,14 @@ class ArtistsController < ApplicationController
     @artist.destroy
     redirect_to @artist, notice: "Artist Deleted!"
   end
+
+  def artist_params
+    params.require(:artist).permit(:name, :image_url)
+  end
+
+  def image_params
+  params[:images].present? ? params.require(:images) : []
+end
 
 
 end
