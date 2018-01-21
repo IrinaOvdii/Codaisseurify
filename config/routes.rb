b35e063d4cfa2044a9b849384331562248b9de24
IrynaOvdii <irina.ovdij@gmail.com>
2018-01-21 07:19:30 +0100

    Last step, fix index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on thRails.application.routes.draw do
   root 'artists#index'
   resources :artists do
-    #delete '/:id/song' => 'songs#destroy', as: :set_artist
     resources :songs
   end
     resources :photos, only: [:destroy]
